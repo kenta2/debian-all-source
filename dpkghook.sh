dccab66778145fb0b07c4ff0722ef0149df2084d
Ken T Takusagawa <kenta@mit.edu>
2022-04-08 18:15:37 -0400

    where to put the hook

--- dpkghook.sh
+++ dpkghook.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# /etc/apt/apt.conf.d/04debianallsource
+#DPkg::Post-Invoke      { "bash /path/to/debian-all-source/dpkghook.sh" ; };
 set -e
 cd /var/lib/debianallsource
 dpkg-query -W -f='(${db:Status-Abbrev}) ${binary:Package} ${Version} ${source:Package} ${source:Version}\n' > dpkg.out
