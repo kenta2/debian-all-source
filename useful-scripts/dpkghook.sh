82fcaf9e82842f39f6304c1ecb06c40f08b2d8b3
Ken T Takusagawa <kenta@mit.edu>
2022-04-10 09:23:25 -0400

    move to useful-scripts dir, and better comments

--- useful-scripts/dpkghook.sh
+++ useful-scripts/dpkghook.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+# keeps a history of dpkg in git in /var/lib
+# user must initialize /var/lib/debianallsource as a git repo and cjeck in an initial dpkg.out, ok if empty
+
+# install dpkg hook:
 # /etc/apt/apt.conf.d/04debianallsource
-#DPkg::Post-Invoke      { "bash /path/to/debian-all-source/dpkghook.sh" ; };
+#DPkg::Post-Invoke      { "bash /path/to/debian-all-source/useful-scripts/dpkghook.sh" ; };
 set -e
 cd /var/lib/debianallsource
 dpkg-query -W -f='(${db:Status-Abbrev}) ${binary:Package} ${Version} ${source:Package} ${source:Version}\n' > dpkg.out
