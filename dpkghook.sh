e8d253bd2c3057d89b4cd0a9e60ad86428bbc40d
Ken T Takusagawa <kenta@mit.edu>
2022-04-09 23:06:00 -0400

    comment

--- dpkghook.sh
+++ dpkghook.sh
@@ -5,3 +5,4 @@ set -e
 cd /var/lib/debianallsource
 dpkg-query -W -f='(${db:Status-Abbrev}) ${binary:Package} ${Version} ${source:Package} ${source:Version}\n' > dpkg.out
 git commit -a -m "$0" || true
+# git fail if nothing to commit
