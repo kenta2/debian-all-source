8b2294b27dcfbfa02aebb5da216361ab96181e38
Ken T Takusagawa <kenta@mit.edu>
2022-04-15 04:20:23 -0400

    comment

--- get1.sh
+++ get1.sh
@@ -20,6 +20,15 @@ d=`mktemp -d`
 pushd "$d"
 apt-get source --only-source "$source=$version" || true NO SOURCE FOUND
 popd
+
+# move the inner directory to our git repository, removing the version
+# suffix.  we figure out what the inner directory is because we expect
+# there to be only one inner directory.  this is a hack.
+
+# the various source tarballs (orig and debianization) and *.dsc above
+# the inner directory are discarded, because they have been
+# incorporated into the inner directory.
+
 if [ "$(find "$d" -mindepth 1 -maxdepth 1 -type d | wc -l)" = 1 ]
 then find "$d" -mindepth 1 -maxdepth 1 -type d -exec mv '{}' "$target/$source" \;
      for special in .gitignore .gitattributes
