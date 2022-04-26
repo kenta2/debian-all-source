0f0a796967b992083970bcc3c168d85cd176d2b5
Ken Takusagawa <kenta@mit.edu>
2022-04-26 17:48:50 -0400

    reorganize without perl -n flag because BEGIN section was getting long

--- sourcesandversions.pl
+++ sourcesandversions.pl
@@ -1,45 +1,43 @@
-#!perl -nlw
-BEGIN{
-    die unless defined$ENV{hardcode};
-    if(defined$ENV{linuxversion}){
-        open FI,$ENV{linuxversion} or die;
-        # when same source, two different versions, hardcode the correct one
-        while(<FI>){
-            @F=split;
-            die unless @F==2;
-            die if$hardcode{$F[0]};
-            $hardcode{$F[0]}=$F[1];
-        }
-    } else {
-        # proceed onward, error may occur later if you have two linux versions installed
-        #die "expecting ENV linuxversion";
+#!perl -lw
+die unless defined$ENV{hardcode};
+if(defined$ENV{linuxversion}){
+    open FI,$ENV{linuxversion} or die;
+    # when same source, two different versions, hardcode the correct one
+    while(<FI>){
+        @F=split;
+        die unless @F==2;
+        die if$hardcode{$F[0]};
+        $hardcode{$F[0]}=$F[1];
     }
+} else {
+    # proceed onward, error may occur later if you have two linux versions installed
+    #die "expecting ENV linuxversion";
 }
-die unless ($installed,$source,$ver)=m/^\(.(.).\) \S+ \S+ (\S+) (\S+)$/;
-next unless $installed eq 'i';
-if(defined($h=$hardcode{$source})){
-    if($h ne $ver){
-        print STDERR "ignoring $_";
-        # pretend the package is not installed.
-    } else {
-        if(defined($pv=$s{$source})){
-            die unless$pv eq $h;
+while(<>){
+    die unless ($installed,$source,$ver)=m/^\(.(.).\) \S+ \S+ (\S+) (\S+)$/;
+    next unless $installed eq 'i';
+    if(defined($h=$hardcode{$source})){
+        if($h ne $ver){
+            print STDERR "ignoring $_";
+            # pretend the package is not installed.
         } else {
-            $s{$source}=$h; # == $ver
+            if(defined($pv=$s{$source})){
+                die unless$pv eq $h;
+            } else {
+                $s{$source}=$h; # == $ver
+            }
         }
-    }
-} else{
-    if(defined($pv=$s{$source})){
-        unless($pv eq $ver){
-            die "collision on $source: $pv and $ver";
+    } else{
+        if(defined($pv=$s{$source})){
+            unless($pv eq $ver){
+                die "collision on $source: $pv and $ver";
+            }
+        } else {
+            $s{$source}=$ver;
         }
-    } else {
-        $s{$source}=$ver;
     }
 }
 
-END{
-    for$p(sort keys%s){
-        print"$p $s{$p}";
-    }
+for$p(sort keys%s){
+    print"$p $s{$p}";
 }
